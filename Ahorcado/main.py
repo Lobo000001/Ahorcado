6138c6b5ef53cad9dd0c66d87a04798910c95967
Lobo000001 <92826045+Lobo000001@users.noreply.github.com>
2025-08-24 22:18:28 -0500

    It added a new game mode.

--- Ahorcado/main.py
+++ Ahorcado/main.py
@@ -1,7 +1,7 @@
 import random
 
 estadisticas = [] 
-
+modo_juego = "clasico"
 
 
 def mostrar_menu():
@@ -16,14 +16,19 @@ def mostrar_menu():
 
 def configuracion():
         #Configuracion
+
+        global modo_juego
+
         print("\n\n--- Configuracion ---")
         print("\n1. Modo clasico")
         print("\n2. Modo oxigeno")
         opcion = input("\n\nElije una opción:")
         
         if opcion == "1":
+            modo_juego = "clasico"
             print("Modo de juego configurado a 'clásico'.")
         elif opcion == "2":
+            modo_juego = "oxigeno"
             print("Modo de juego configurado a 'oxígeno'.")
         else:
             print("Opción inválida, no se cambió el modo.")
@@ -64,23 +69,35 @@ def jugar_denuevo():
 
 def jugar():
         #Comienza el juego
+
+        global modo_juego
+
         palabras = ("python", "ahorcado", "programa", "computadora", "laptop", "musica", "encebollado", "plotter")
         palabra_secreta = random.choice(palabras)
         letras_adivinadas = set()
+
         intentos = 6
-        print("\n\nTienes 6 intentos")
-        while intentos > 0:
+        oxigeno = 100
+        perdida = 20
+
+        if modo_juego == "clasico":
+            print("\n\nTienes 6 intentos para adivinar la palabra")
+        else:
+            print("\n\nModo oxígeno activado: tienes 100% de oxígeno.")
+            print("Cada error reduce tu oxígeno en %d%%." %(perdida))
+
+        while True:
             estado_palabra = ""
             for letra in palabra_secreta:
                 if letra in letras_adivinadas:
-                    estado_palabra += letra + " "
+                        estado_palabra += letra + " "
                 else:
-                    estado_palabra += "_ "
+                        estado_palabra += "_ "
 
             print("\n Palabra:", estado_palabra.strip())
- 
+    
             letra_usuario = input("\nIngresa una letra: ").lower().strip()
-                
+                    
             if len(letra_usuario) != 1 or not letra_usuario.isalpha():
                 print("\nPor favor, ingresa solo una letra válida.")
                 continue
@@ -89,18 +106,28 @@ def jugar():
                 continue
             letras_adivinadas.add(letra_usuario)
             if letra_usuario in palabra_secreta:
-                print("\n¡Bien! La letra está en la palabra.")
-                
+                    print("\n¡Bien! La letra está en la palabra.")
+                    
             else:
-                intentos -= 1
-                print("\nLa letra no está. Te quedan %d intentos." %(intentos))
+                if modo_juego == "clasico":
+                    intentos -= 1
+                    print("\nLa letra no está. Te quedan %d intentos." %(intentos))
+                    if intentos == 0:
+                        print("\n¡Te han colgado! La palabra correcta era: %s" %(palabra_secreta))
+                        estadisticas.append((palabra_secreta, "Perdiste"))
+                        break
+                else:
+                    oxigeno -= perdida
+                    print("\nLa letra no está. Te quedan %d%% oxigeno." %(oxigeno))
+                    if oxigeno <= 0:
+                        print("\n¡Te quedaste sin oxigeno! La palabra correcta era: %s" %(palabra_secreta))
+                        estadisticas.append((palabra_secreta, "Perdiste"))
+                        break
             if all(letra in letras_adivinadas for letra in palabra_secreta):
-                print("\n¡Felicidades! Adivinaste la palabra: %s" %(palabra_secreta))
-                estadisticas.append((palabra_secreta, "Ganaste"))
-                break
-        else:     
-            print("\n¡Perdiste! La palabra correcta era: %s" %(palabra_secreta))
-            estadisticas.append((palabra_secreta, "Perdiste"))
+                        print("\n¡Felicidades! Adivinaste la palabra: %s" %(palabra_secreta))
+                        estadisticas.append((palabra_secreta, "Ganaste"))
+                        break
+
         decision = jugar_denuevo()
         if decision == "jugar":
             jugar()  # llamas a jugar otra vez
